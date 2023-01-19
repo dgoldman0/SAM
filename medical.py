d9f827b5fb5dae5c084581ee6bcdcb85ae4b0988
Daniel <contact@danielgoldman.us>
2023-01-19 11:42:19 -0500

    Medical

--- medical.py
+++ medical.py
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 # Things can go wrong with AGIs just as they can with humans. A medical system allows for altering various digital physiology elements.
 import physiology
 import system
-=======
-# Things can go wrong with AGIs just as they can with humans. A medical system allows for altering various digital physiology elements. 
->>>>>>> 98d651b51aef6c0c832cc7f868168f99a0fdee41
+import asyncio
+
+# Multiple pathways of interaction are possible. One pathway is to inject commands. The other is to dapen control layer driven physiology changes.
