4c033377b118c0bc470e66a84d02a948d0c0b426
Daniel <contact@danielgoldman.us>
2023-02-06 09:33:13 -0500

    Reset memory.

--- SAM1A/data.py
+++ SAM1A/data.py
@@ -55,6 +55,7 @@ def init():
         if str(err) == "no such table: USERS":
             # Create user table and add the administrator.
             cur.execute("CREATE TABLE USERS(user_id INT PRIMARY KEY, username TEXT NOT NULL, display_name TEXT NOT NULL, passwd TEXT NOT NULL, salt TEXT NOT NULL, admin INT DEFAULT FALSE, blocked NOT NULL DEFAULT FALSE);")
+            username = input("Enter admin username: ")
             salt = bcrypt.gensalt()
             admin_password = "not"
             confirm = "same"
@@ -62,7 +63,7 @@ def init():
                 admin_password = input("Enter admin password: ")
                 confirm = input("Confirm password: ")
             password = bcrypt.hashpw(admin_password.encode(), salt)
-            cur.execute("INSERT INTO USERS (username, display_name, passwd, salt, admin) VALUES (?, ?, ?, ?, ?);", ("admin", "System Administrator", password, salt, True))
+            cur.execute("INSERT INTO USERS (username, display_name, passwd, salt, admin) VALUES (?, ?, ?, ?, ?);", (username, "System Administrator", password, salt, True))
             database.commit()
         else:
             raise Exception("Unknown Error")
