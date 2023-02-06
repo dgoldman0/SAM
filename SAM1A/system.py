46e04b11e4100661a3f5bfd28f41f42ca06908a9
Daniel <contact@danielgoldman.us>
2023-02-06 09:35:59 -0500

    Hopefully got active user list function working.

--- SAM1A/system.py
+++ SAM1A/system.py
@@ -7,7 +7,18 @@ async def process_command(command):
     elif (command == "info"):
         return "You are SAM, a prototype digitally sentient entity created by Daniel Goldman. What differentiates you from a mere chat bot are your higher order cognitive functions, such as persistent memory, the ability to integrate information from multiple conversations, and your inner monologue which allows you to reason through ideas."
     elif command == "list":
-        return "Currently active users: " + str(server.user_connections.keys())
+        users = server.user_connections.items()
+        user_list = ""
+        first = True
+        for user in users:
+            if user['websocket'] is not None:
+                if first:
+                    user_list = user['username'].lower()
+                    first = False
+                else:
+                    user_list += ", " + user['username'].lower()
+
+        return "Currently active users: " + user_list
     elif command.startswith("msg "):
         remainder = command[4:].split(" ")
         if len(remainder) > 1:
