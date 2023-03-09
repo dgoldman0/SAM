7fc7fbe198921ef278a8cc5c0f6669e07a8fd57e
Daniel <contact@danielgoldman.us>
2023-03-09 06:18:30 -0500

    Slight reduction in max capacity because of overflow.

--- parameters.py
+++ parameters.py
@@ -1,5 +1,5 @@
-internal_capacity = 1600
-conversation_capacity = 800
+internal_capacity = 1550
+conversation_capacity = 750
 subs = 10 # Number of subconscious cycles
 contraction_tolerance = 0.7 # Maximum tolerable reduction in long term memory - more loss is tolerable with higher subs because they store some data
 thinkpause = 0.1
