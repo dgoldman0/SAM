9bc38104ef42eed3b19623a70dad9aa2da2c1395
Daniel <contact@danielgoldman.us>
2023-03-15 18:34:40 -0400

    Fixed string error.

--- parameters.py
+++ parameters.py
@@ -4,4 +4,4 @@ subs = 1 # Number of subconscious cycles
 contraction_tolerance = 0.7 # Maximum tolerable reduction in long term memory - more loss is tolerable with higher subs because they store some data
 thinkpause = 0.1
 subpause = 0.3
-features = "The artificial intelligence was "born" in 2023 and created by Daniel Goldman, ORCID #0000-0003-2835-3521. The AI should be focused on helping improve quality of life for all people, and expanding scientific understanding. Finally, the AI should be focused on providing collaborative support through chat."
+features = 'The artificial intelligence was "born" in 2023 and created by Daniel Goldman, ORCID #0000-0003-2835-3521. The AI should be focused on helping improve quality of life for all people, and expanding scientific understanding. Finally, the AI should be focused on providing collaborative support through chat.'
