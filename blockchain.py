1ebc7bf4e449811845c99e14c28370fa17e9e206
Daniel <contact@danielgoldman.us>
2023-03-24 10:55:23 -0400

    Adding more functions.

--- blockchain.py
+++ blockchain.py
@@ -5,13 +5,62 @@ from web3 import Web3
 load_dotenv()
 
 private_key = os.getenv('PRIVATE_KEY')
+api_key = os.getenv('INFURA_API_KEY')
 
-default_provider = 'https://goerli.infura.io/v3/YOUR-API-KEY'
+default_provider = 'https://goerli.infura.io/v3/' + api_key
 
 w3 = Web3(Web3.HTTPProvider(default_provider))
 
 def get_nonce():
-    nonce = web3.eth.getTransactionCount(from_account)
+    nonce = w3.eth.getTransactionCount(w3.eth.defaultAccount)
+    return nonce
 
 def check_gas(tx):
-    pass
+    gas = web3.eth.estimateGas(tx)
+    return "Estimated gas fee for tx: " + gas
+
+def send_transaction(tx):
+    signed_tx = w3.eth.account.signTransaction(tx, private_key=private_key)
+    tx_hash = w3.eth.sendRawTransaction(signed_tx.rawTransaction)
+    return "Transaction hash: " + tx_hash.hex()
+
+def get_balance(address):
+    balance = w3.eth.getBalance(address)
+    return "Balance for address " + address + ": " + balance
+
+def get_transaction(tx_hash):
+    tx = w3.eth.getTransaction(tx_hash)
+    return tx
+
+def get_transaction_receipt(tx_hash):
+    tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
+    return "Transaction receipt for tx " + tx_hash + ": " + tx_receipt
+
+def get_contract(address):
+    contract = w3.eth.contract(address=address)
+    return contract
+
+def get_contract_abi(address):
+    contract = w3.eth.contract(address=address)
+    abi = contract.abi
+    return "ABI for contract at address " + address + ": " + abi
+
+def get_contract_bytecode(address):
+    contract = w3.eth.contract(address=address)
+    bytecode = contract.bytecode
+    return bytecode
+
+def get_contract_events(address):
+    contract = w3.eth.contract(address=address)
+    events = contract.events
+    return events
+
+def get_contract_functions(address):
+    contract = w3.eth.contract(address=address)
+    functions = contract.functions
+    return functions
+
+def get_contract_call(address, function, *args):
+    contract = w3.eth.contract(address=address)
+    call = contract.functions.function(*args).call()
+    return call
